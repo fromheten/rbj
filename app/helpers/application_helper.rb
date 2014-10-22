88e5fd2bcc724da87905772e357ec0c13d2277b4
Martin Josefsson <josefsson.martin@gmail.com>
2014-10-22 18:30:27 +0200

    Using the centralized WHITELISTED_TAGS constant for every list of tags, dryed up!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def tags_with_formatting
+    formatted_list = []
+    WHITELISTED_TAGS.each do |t|
+      formatted_list.push([t.titlecase, t])
+    end
+    return formatted_list
+  end
 end
--- config/initializers/tag_whitelist.rb
+++ config/initializers/tag_whitelist.rb
@@ -1,4 +1,4 @@
 # This is the whitelist for tags, also known as categories.
 # Remember to restart the app after editing this file
 
-WHITELISTED_TAGS = ["programming", "design", "copywriting"]
+WHITELISTED_TAGS = ["programming", "design", "copywriting", "bussiness/exec", "support", "marketing", "sales"]
